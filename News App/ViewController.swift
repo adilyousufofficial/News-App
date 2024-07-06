70c924841901a4d987106899e1b1cbc0bb7f80e6
M Adil <adilyousufofficial@gmail.com>
2024-07-07 00:44:43 +0500

    Initial commit

---
+++ News App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by Adil Yousuf on 07/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
